f61eb037f60e8abb462b96593fbd62cd2b6bd299
Chris Mason <c.mason@inchipdesign.com.au>
2018-11-04 14:30:40 +1100

    enable usb

--- ports/stm32/boards/CNX-DIP40-F405CA/mpconfigboard.h
+++ ports/stm32/boards/CNX-DIP40-F405CA/mpconfigboard.h
@@ -1,3 +1,4 @@
+#define MICROPY_HW_USB_FS              (1)
 #define MICROPY_HW_BOARD_NAME       "CNX40-F405CA"
 #define MICROPY_HW_MCU_NAME         "STM32F405RG"
 
@@ -5,13 +6,12 @@
 #define MICROPY_HW_HAS_FLASH        (1)
 #define MICROPY_HW_HAS_SDCARD       (0)
 #define MICROPY_HW_HAS_MMA7660      (0)
-#define MICROPY_HW_HAS_LIS3DSH      (0)
 #define MICROPY_HW_HAS_LCD          (0)
 #define MICROPY_HW_ENABLE_RNG       (1)
 #define MICROPY_HW_ENABLE_RTC       (1)
-#define MICROPY_HW_ENABLE_TIMER     (1)
-#define MICROPY_HW_ENABLE_SERVO     (0)
+#define MICROPY_HW_ENABLE_SERVO     (1)
 #define MICROPY_HW_ENABLE_DAC       (1)
+#define MICROPY_HW_ENABLE_USB       (1)
 //#define MICROPY_HW_ENABLE_CAN       (1)
 
 // HSE is 12MHz
@@ -98,5 +98,6 @@
 //#define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
 
 // USB config
+#define MICROPY_HW_USB_FS              (1)
 //#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
 //#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
